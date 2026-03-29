4bb7e5acab8983e3225a0a132fa99bd542048b39
Kai Salmon <kaisalmon@hotmail.com>
2026-03-29 18:15:01 +0100

    Better test env, better death animQ

---
+++ gym/gym.gd
@@ -0,0 +1,9 @@
+extends Node
+
+func _ready():
+	Globals.getPlayer().in_cutscene = false
+
+func _process(_delta):
+	var player = Globals.getPlayer()
+	RenderingServer.global_shader_parameter_set("planet_radius", 30.0)
+	RenderingServer.global_shader_parameter_set("curve_origin", player.global_position)
