23332c30d87ca8d96ad8d8d605a554873f113b13
andreyslmv <andrey@sulimov.net>
2026-04-01 20:39:13 +0200

    combat music test pushed

--- music_manager.gd
+++ music_manager.gd
@@ -4,19 +4,40 @@ extends Node
 @onready var gameplay_music: AudioStreamPlayer = %GameplayMusic
 @onready var credits_music: AudioStreamPlayer = %CreditsMusic
 
+const mute = -60
+const unmute = 10
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	play_music()
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+var is_fading_combat = false
 
-func play_music(string):
+func _process(delta: float) -> void:
+	if Globals.in_combat and not is_fading_combat:
+		is_fading_combat = true
+		tween_combat_in()
+	elif not Globals.in_combat and is_fading_combat:
+		is_fading_combat = false
+		tween_combat_out()
+
+func play_music():
+	gameplay_music.play()
 	
-	
+
+func music_volume(vol: float) -> void:
+	gameplay_music.stream.set_sync_stream_volume(1, vol)
+
+func tween_combat_in() -> void:
+	var combat_fade_in = create_tween()
+	combat_fade_in.tween_method(Callable(self, "music_volume"), mute, unmute, 1)
+
+func tween_combat_out() -> void:
+	var combat_fade_out = create_tween()
+	combat_fade_out.tween_method(Callable(self, "music_volume"), unmute, mute, 1)
 
 func music_stop():
 	var fadeout = create_tween() #Tween for smooth music fadeouts
