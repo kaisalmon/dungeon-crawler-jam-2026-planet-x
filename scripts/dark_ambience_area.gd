a6f37150c10a3a9436f3f2a5ff16407325384445
Kai Salmon <kaisalmon@hotmail.com>
2026-04-10 21:17:34 +0100

    better cave

--- scripts/dark_ambience_area.gd
+++ scripts/dark_ambience_area.gd
@@ -5,6 +5,8 @@ var ambient_color
 var transition = 0.0
 var directional_light_strength
 var sun_strength
+var children_light_strengths = []
+var children_lights = []
 
 func _ready():
 	ambient_color = get_world_3d().environment.ambient_light_color
@@ -12,6 +14,10 @@ func _ready():
 	sun_strength = %Sun.light_energy
 	connect("body_entered", Callable(self, "_on_body_entered"))
 	connect("body_exited", Callable(self, "_on_body_exited"))
+	for child in get_children():
+		if child is Light3D:
+			children_lights.append(child)
+			children_light_strengths.append(child.light_energy)
 
 func _on_body_entered(body: Node3D) -> void:
 	print("entered", body.name)
@@ -34,6 +40,9 @@ func _process(delta: float) -> void:
 
 	var global_directional_light = %DirectionalLight3D
 	global_directional_light.light_energy = directional_light_strength * (1.0 - transition)
+
+	for i in range(len(children_lights)):
+		children_lights[i].light_energy = children_light_strengths[i] * (transition)
 
 	var sun = %Sun
 	sun.light_energy = sun_strength * (1.0 - transition)
